8d74e3176cf2904986bb918cd61950e510a6c628
Felipe-HVSilva <felype.hen@hotmail.com>
2023-04-10 18:34:42 -0300

    :bug: block scroll when modal is opened

--- js/modal.js
+++ js/modal.js
@@ -4,11 +4,23 @@ const $buttonCloseModal = document.querySelector(".close-modal")
 const $modalOverlay = document.querySelector(".overlay")
 const $modal = document.querySelector(".modal")
 
+function unloadScrollBars() {
+  document.documentElement.style.overflow = "hidden"
+  document.body.scroll = "no" // IE
+}
+
+function reloadScrollBars() {
+  document.documentElement.style.overflow = "auto"
+  document.body.scroll = "yes" // IE
+}
+
 function closeModal() {
+  reloadScrollBars()
   $modal.classList.remove("active")
 }
 
 function openModal() {
+  unloadScrollBars()
   $modal.classList.add("active")
 }
 
